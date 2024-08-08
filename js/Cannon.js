090b8fcd4345d533caf55973bdedbf2845f32fab
Dkingnoprime <diudestroi@gmail.com>
2024-08-08 18:05:05 -0300

    Aula 26
    Upgrade of the pirate game

--- js/Cannon.js
+++ js/Cannon.js
@@ -5,18 +5,29 @@ class Cannon {
     this.width = width;
     this.height = height;
     this.angle = angle;
-    this.cannon_image = loadImage("assets/canon.png");
-    this.cannon_base = loadImage("assets/cannonBase.png");
+    this.cannon = loadImage("assets/canon.png") 
+    this.base = loadImage ("assets/cannonBase.png")
   }
-  display() {
-    
+
+  display(){
+    if(keyIsDown(UP_ARROW)&& this.angle>-40){
+      this.angle-=1
+    }
+    if(keyIsDown(DOWN_ARROW)&& this.angle<70){
+      this.angle+=1
+    }
     push();
-    translate(this.x, this.y);
+    translate(this.x,this.y);
     rotate(this.angle);
-    imageMode(CENTER);
-    image(this.cannon_image, 0, 0, this.width, this.height);
+    imageMode(CENTER)
+    image(this.cannon,0,0,this.width, this.height);
     pop();
-    image(this.cannon_base, 70, 20, 200, 200);
-    noFill();
+
+    //base do canhao
+      image(this.base, 110,75,170,150);
+      noFill()
+
   }
+
+  
 }
--- sketch.js
+++ sketch.js
@@ -2,46 +2,118 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-
 var engine, world, backgroundImg;
-var canvas, angle, tower, ground, cannon;
-var cannonBall;
 
+var canvas, angle, tower, ground, cannon;
+var balls=[]
+var boats=[]
 function preload() {
   backgroundImg = loadImage("./assets/background.gif");
   towerImage = loadImage("./assets/tower.png");
+  
 }
 
 function setup() {
+
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-  angle = 20
+  
+  var options = {
+    isStatic: true
+  }
 
-  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
   World.add(world, ground);
 
-  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+  tower = Bodies.rectangle(160, 350, 160, 310, options);
   World.add(world, tower);
-
-  cannon = new Cannon(180, 110, 130, 100, angle);
-  cannonBall = new CannonBall(cannon.x, cannon.y);
+  angleMode(DEGREES)
+  angle=15;
+  cannon=new Cannon(220,140,170,170,angle);
+  
+ 
 }
 
 function draw() {
-  background(189);
-  image(backgroundImg, 0, 0, width, height);
-
+  image(backgroundImg,0,0,1200,600)
   Engine.update(engine);
 
+  
   rect(ground.position.x, ground.position.y, width * 2, 1);
+  
+
   push();
   imageMode(CENTER);
-  image(towerImage, tower.position.x, tower.position.y, 160, 310);
-  pop();
-
-  cannon.display();
-  cannonBall.display();
+  image(towerImage,tower.position.x, tower.position.y, 160, 310);
+  pop();  
+  
+  for (var i=0; i<balls.length; i++) {
+    showCannonBalls(balls[i],i)
+    colisionWithBoats(i);
+  }
+  cannon.display()
+  showBoats()
 }
 
+function keyReleased(){
+  if (keyCode===32) {
+    balls[balls.length-1].shoot();
+    
+  }
+}
 
+function keyPressed(){
+  if (keyCode===32) {
+    cannonBall=new CannonBall(cannon.x,cannon.y);
+    balls.push(cannonBall)
+  }
+}
+
+function showCannonBalls(ball,i) {
+  if (ball){
+    ball.display()
+    if (ball.body.position.x>=width || ball.body.position.y>=height-50) {
+      ball.remove(i)
+    }
+  }
+}
+
+function showBoats() {
+  if (boats.length>0){
+    if (boats[boats.length-1]===undefined || boats[boats.length -1].body.position.x<width-300) {
+      var positions=[-20,-40,-60,-70];
+      position=random(positions);
+      boat=new Boat(width,height-100,170,170,position);
+      boats.push(boat)
+    }
+
+    for(var i=0; i<boats.length;i++){
+      if (boats[i]) {
+        Matter.Body.setVelocity(boats[i].body,{x:-0.9, y:0})
+        boats[i].display()
+      }
+      else{
+        boats[i]
+      }
+      
+    }
+  }
+  else{
+    boat=new Boat(width-79,height-60,170,170,-80);
+    boats.push(boat)
+  }
+}
+
+function colisionWithBoats(index) {
+  for(var i=0; i<boats.length; i++) {
+    if (balls[index]!==undefined && boats[i]!=undefined) {
+      var colision= Matter.SAT.collides(balls[index].body,boats[i].body)
+      if (colision.collided){
+        boats[i].remove(i)
+        Matter.World.remove(world,balls[index].body)
+        delete balls[index]
+      }
+    }
+  }
+}
